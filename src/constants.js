a51df20da5ef717083bbe2979f26eb7dbd29f855
Lipkind <ethan.lipkind@nytimes.com>
2019-09-25 11:25:01 -0400

    Aliaksandr -> Alex

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const names = ['Ethan', 'Ali', 'Daniel', 'Misha', 'Uma', 'Aliaksandr', 'Matt', 'Jessie', 'Sritama', 'Ahmed', 'Eugene', 'Ash'];
+export const names = ['Ethan', 'Ali', 'Daniel', 'Misha', 'Uma', 'Alex', 'Matt', 'Jessie', 'Sritama', 'Ahmed', 'Eugene', 'Ash'];
 export const modes = {
     confirm_participants: 'CONFIRMING_PARTICIPANTS',
     confirm_squad_leads: 'CONFIRMING_SQUAD_LEADS',
